92334b6edf9fc2ebb6d57b0e350227ff21e8cac7
Hayden <hayden00715@yahoo.ca>
2024-07-03 17:28:56 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import CreateEvent from './pages/CreateEvent';
@@ -10,12 +10,12 @@ function App() {
   return (
     <Router>
       <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/create-event" component={CreateEvent} />
-          <Route path="/explore" component={Explore} />
-          <Route path="/chat" component={Chat} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-event" element={<CreateEvent />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
       </Layout>
     </Router>
   );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-
+// src/components/Layout.js
 import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 
@@ -7,10 +7,10 @@ const Layout = ({ children }) => (
     <Navbar bg="dark" variant="dark">
       <Navbar.Brand href="/">GamePlan</Navbar.Brand>
       <Nav className="mr-auto">
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/create-event">Create Event</Nav.Link>
-        <Nav.Link href="/explore">Explore</Nav.Link>
-        <Nav.Link href="/chat">Chat</Nav.Link>
+        <Nav.Link as={Link} to="/">Home</Nav.Link>
+        <Nav.Link as={Link} to="/create-event">Create Event</Nav.Link>
+        <Nav.Link as={Link} to="/explore">Explore</Nav.Link>
+        <Nav.Link as={Link} to="/chat">Chat</Nav.Link>
       </Nav>
     </Navbar>
     <Container>

d1951ffe2c7ee1f8675af3997ebbed99bf837bde
Hayden <hayden00715@yahoo.ca>
2024-07-21 16:04:50 -0400

    Commit message for new changes

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
-
+//Test
 const Signup = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
